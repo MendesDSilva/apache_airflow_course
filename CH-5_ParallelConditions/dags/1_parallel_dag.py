3bf0e38e0cee2cfe7569fd85a3ebeb7794f30e6e
Mateus Mendes da Silva <mendonsa_23@hotmail.com>
2026-08-31 00:26:37 -0300

    Update

---
+++ CH-5_ParallelConditions/dags/1_parallel_dag.py
@@ -0,0 +1,42 @@
+from airflow.sdk import dag, task
+
+@dag
+def parallel_dag():
+
+    @task.bash
+    def task_bash():
+
+        return "echo 'Hello from Bash'"
+
+    @task.python
+    def fetch_api():
+        data = {"type":"api","data": ["data1", "data2", "data2"]}
+        return data
+
+    @task.python
+    def fetch_db():
+        data = {"type":"db","data": ["data4", "data5", "data6"]}
+        return data 
+
+    @task.python()
+    def fetch_s3():
+        data = {"type":"s3","data": ["data7", "data8", "data9"]}
+        return data
+
+    @task.python
+    def process_data(api_data, db_data, s3_data):
+        print("Processing API Data:", api_data)
+        print("Processing API DB:", db_data)
+        print("Processing API S3 Data:", s3_data)
+
+
+    task_bash = task_bash()
+    api_data = fetch_api()
+    db_data = fetch_db()
+    s3_data = fetch_s3()
+
+    # Dependencies 
+
+    task_bash >> [api_data, db_data, s3_data] >> process_data(api_data, db_data, s3_data)
+
+parallel_dag = parallel_dag()
