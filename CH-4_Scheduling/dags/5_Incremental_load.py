e501f94187a49ee7c72c1bc808087939f9e21844
Mateus Mendes da Silva <mendonsa_23@hotmail.com>
2026-08-30 22:50:05 -0300

    Adiciona aula incremental load

--- CH-4_Scheduling/dags/5_Incremental_load.py
+++ CH-4_Scheduling/dags/5_Incremental_load.py
@@ -3,7 +3,7 @@ from airflow.timetables.interval import CronDataIntervalTimetable
 import pendulum
 
 @dag(
-        schedule = CronDataIntervalTimetable("0 0 * * *", tz="America/Sao_Paulo"),
+        schedule = CronDataIntervalTimetable("0 0 * * *", timezone="America/Sao_Paulo"),
         start_date=pendulum.datetime(year=2026, month=4, day=15, tz="America/Sao_Paulo"),
         catchup = True
 )
@@ -13,8 +13,8 @@ def incremental_load():
     def extract_data(**kwargs):
 
         #Extracting from and to dates
-        from_date = kwargs['date_interval_start']
-        to_date = kwargs['date_interval_end']
+        from_date = kwargs['data_interval_start']
+        to_date = kwargs['data_interval_end']
 
         # Simulate extracting data from a source
         print(f"Extracting data from {from_date} to {to_date}")
@@ -26,7 +26,7 @@ def incremental_load():
 
 
         return """
-        "echo Data Loaded from {{date_interval_start | ds}} to {{date_interval_end | ds}}"
+        "echo Data Loaded from {{data_interval_start | ds}} to {{data_interval_end | ds}}"
         """
 
     extract_data() >> load_data()
