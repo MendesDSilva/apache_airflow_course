7305fa44e79435ec9dca6dc5ca0fd7c617e6107b
Mateus Mendes da Silva <mendonsa_23@hotmail.com>
2026-09-08 02:02:51 -0300

    Atualização

--- CH-12_AWS_Project/dags/aws_project.py
+++ CH-12_AWS_Project/dags/aws_project.py
@@ -1,6 +1,8 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__)),'..')
+sys.path.append(
+    os.path.join(os.path.dirname(__file__), '..')
+)
 from utils.bronze_layer import BronzeLayer
 from airflow.sdk import task, dag
 from datetime import timedelta
